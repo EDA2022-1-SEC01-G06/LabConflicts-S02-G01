3f671a01c7e667017bb930bce763f876f1069755
Ana <annamadelblanc@gmail.com>
2022-02-08 10:33:58 -0500

    cambios del Estudiante 1

--- App/model.py
+++ App/model.py
@@ -75,4 +75,3 @@ def createTagList():
 def addBookTags():
     # TODO: Modificación de Est-1 y Est-2 en el Lab 2
     booktags= lt.newList(datastructure='SINGLE_LINKED', filename=booktagsfile)
-    
